62c504db1de538d490cea3e97000f5fa1e647de9
Muhammad Awais <awaisoo668@gmail.com>
2024-10-06 17:49:12 +0500

    Add files via upload

---
+++ Task 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1=50,n2=25;
+    float div=n1/n2;
+    cout <<"The sum of two number is:"<<n1+n2<<endl;
+    cout<<"The subtraction of two number is:"<<n1-n2<<endl;
+    cout <<"The product of two number is:"<<n1*n2<<endl;
+    cout<<"The division of two number is :"<<div<<endl;
+
+}
