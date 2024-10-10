6ee2479a7e9d723ba3d1508d39b56ce5a1d6f0d3
Muhammad Awais <awaisoo668@gmail.com>
2024-10-10 22:01:11 +0500

    Add my program on Github

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    cout<<"Sisyed Case Institute of Technology Islamabad.";
+
+int main() {
+    char operation;
+    float num1, num2;
+
+    // Display available operations
+    cout << "Select an operation to perform (+, -, *, /): ";
+    cin >> operation;
+
+    // Input two numbers
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    // Perform the selected operation
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Error: Division by zero is not allowed." << endl;
+            break;
+        default:
+            cout << "Error: Invalid operation selected!" << endl;
+            break;
+    }
+
     return 0;
 }
