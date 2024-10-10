3b48bbb7d414aec3d569e3619081ed84c76accfe
Muhammad Awais <awaisoo668@gmail.com>
2024-10-10 22:14:51 +0500

    Add my calculator

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,36 +3,37 @@ using namespace std;
 
 int main() {
     char operation;
-    float num1, num2;
+    float num1, num2, result;
 
-    // Display available operations
+    // Input operation and numbers from the user
     cout << "Select an operation to perform (+, -, *, /): ";
     cin >> operation;
-
-    // Input two numbers
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
-    // Perform the selected operation
-    switch (operation) {
-        case '+':
-            cout << "Result: " << num1 + num2 << endl;
-            break;
-        case '-':
-            cout << "Result: " << num1 - num2 << endl;
-            break;
-        case '*':
-            cout << "Result: " << num1 * num2 << endl;
-            break;
-        case '/':
-            if (num2 != 0)
-                cout << "Result: " << num1 / num2 << endl;
-            else
-                cout << "Error: Division by zero is not allowed." << endl;
-            break;
-        default:
-            cout << "Error: Invalid operation selected!" << endl;
-            break;
+    // Perform the selected operation using if-else statements
+    if (operation == '+') {
+        result = num1 + num2;
+        cout << "Result: " << result << endl;
+    } 
+    else if (operation == '-') {
+        result = num1 - num2;
+        cout << "Result: " << result << endl;
+    } 
+    else if (operation == '*') {
+        result = num1 * num2;
+        cout << "Result: " << result << endl;
+    } 
+    else if (operation == '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+            cout << "Result: " << result << endl;
+        } else {
+            cout << "Error: Division by zero is not allowed." << endl;
+        }
+    } 
+    else {
+        cout << "Error: Invalid operation!" << endl;
     }
 
     return 0;
